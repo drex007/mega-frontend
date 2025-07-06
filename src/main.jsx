dabb071e6252273ca2b5a780be7a50a728ce54d4
drex007 <vykeoj@gmail.com>
2025-07-06 22:55:02 +0100

    feat: updated main file

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,6 @@ import { Toaster } from 'react-hot-toast'
 import { store } from './redux/store.jsx'
 import { Provider } from 'react-redux'
 
-
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>

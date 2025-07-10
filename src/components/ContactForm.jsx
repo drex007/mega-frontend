7dedd4ff20afd56d68316774b43ed89a370f2ea9
drex007 <vykeoj@gmail.com>
2025-07-10 14:19:42 +0100

    feat: scroll navigations, contact form and mobile responsiveness

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -4,10 +4,10 @@ const ContactForm = () => {
     return (
         <div className='w-full py-4 px-4 font-neulis' id='contactus'>
             <p className='text-[25px]'>Contact Us</p>
-            <input type="text" name="text" id="" className='p-4 w-full my-4 border-2 border-black' placeholder='Email address' />
-            <input type="text" name="text" id="" className='p-4 w-full my-4 border-2 border-black' placeholder='Phonenumber' />
-            <input type="number" name="text" id="" className='p-4 w-full my-4 border-2 border-black' placeholder='Location' />
-            <textarea name="" id="" cols={10} rows={10} className='p-4 w-full my-4 border-2 border-black' placeholder='Message'></textarea>
+            <input type="text" name="text" id="" className='p-4 w-full my-4 border-[1px] rounded-md border-black' placeholder='Email address' />
+            <input type="text" name="text" id="" className='p-4 w-full my-4 border-[1px] rounded-md border-black' placeholder='Phonenumber' />
+            <input type="number" name="text" id="" className='p-4 w-full my-4 border-[1px] rounded-md border-black' placeholder='Location' />
+            <textarea name="" id="" cols={10} rows={10} className='p-4 w-full my-4 border-[1px] rounded-md border-black' placeholder='Message'></textarea>
             <button className=" bg-primary-dark-green text-white w-full rounded-lg py-4 justify-center">Submit Form</button>
 
 
